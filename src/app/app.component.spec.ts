da8fa0c799e6447752cc80a681dd47e0be26ee7c
jrevol <jrevol@financeactive.com>
2017-07-13 11:03:49 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,10 +19,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Johanna'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Charlotte');
+    expect(app.title).toEqual('Johanna');
   }));
 
 });
